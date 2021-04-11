b641265cd4381a9cad4e5c3bf72b954ad5b8a3dd
Corey Ladovsky <coreyskymusic@gmail.com>
2021-04-11 12:51:18 -0700

    add data

---
+++ src/data/studentData.js
@@ -0,0 +1,53 @@
+const studentData = [
+    {
+      "id": "36bb1ef5-b3fe-4646-ab48-97716812831d",
+      "name": "Rick Sanchez",
+      "age": 72,
+      "bio": "I am a super genius inventor. Nobody is better than me. I can't believe Jerry married my daughter",
+      "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+      "GPA": 4.6
+    },
+    {
+      "id": "02aa9794-ca78-4d5e-a10c-fa0568190955",
+      "name": "Morty Smith",
+      "age": 14,
+      "bio": "I'm just a regular 14 year old you know. I like going on adventures with grandpa. I love Jessica.",
+      "image": "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+      "GPA": 2.05
+    },
+    {
+      "id": "05b4aa6b-55a1-4579-acc0-582f21d2a017",
+      "name": "Summer Smith",
+      "age": 16,
+      "bio": "I should be able to do anything I want. Morty's always off with grandpa and no one gives him any $*#% for it.",
+      "image": "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+      "GPA": 3.3
+    },
+    {
+      "id": "a03ee78c-61aa-46ba-a789-4da549a87280",
+      "name": "Beth Smith",
+      "age": 35,
+      "bio": "I am a surgeon! I love you dad.",
+      "image": "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+      "GPA": 4.0
+    },
+    {
+      "id": "716cfc96-afb7-4476-bed4-bcf5ce0c3a4d",
+      "name": "Jerry Smith",
+      "age": 34,
+      "bio": "blah blah blah, that's anyone hears. Well, I'm trying my best!",
+      "image": "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
+      "GPA": 1.72
+
+    },
+    {
+      "id": "4f5ef1ad-4fec-4059-bb83-6560b171cc55",
+      "name": "Abadango Cluster Princess",
+      "age": 100,
+      "bio": "In possession of the plasma shard",
+      "image": "https://rickandmortyapi.com/api/character/avatar/6.jpeg",
+  
+    }
+  ];
+
+export default studentData;
