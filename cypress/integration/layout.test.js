0f4c85cc663ec5f7995cdaa08b7991a98c02f579
Corey Ladovsky <coreyskymusic@gmail.com>
2021-04-12 11:09:28 -0700

    create layout.test.js

--- cypress/integration/layout.test.js
+++ cypress/integration/layout.test.js
@@ -1,34 +1,33 @@
-import productData from "../../src/data/productData";
+import studentData from "../../src/data/studentData";
 
 describe("layout", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000");
   });
 
-  it("has a section with products laid out in a grid", () => {
-    cy.get(".Products").should("have.css", "display", "grid");
-    cy.get(".Products").should("have.css", "grid-template-columns");
+  it("has a section with students laid out in a grid", () => {
+    cy.get(".Students").should("have.css", "display", "grid");
+    cy.get(".Students").should("have.css", "grid-template-columns");
 
-    productData.forEach((product) => {
-      const { name } = product;
+    studentData.forEach((student) => {
+      const { name } = student;
       cy.findByText(name).should("exist");
     });
   });
 
-  it("has a cart section with subtotal, tax, and total", () => {
-    cy.findByText("Cart").should("exist");
-    cy.findByText(/Subtotal/).should("exist");
-    cy.findByText(/Tax/).should("exist");
-    cy.findByText(/Total/).should("exist");
+  it("has an enrolled section with student count, and average GPA", () => {
+    cy.findByText("Enrolled").should("exist");
+    cy.findByText(/Total Student Count/).should("exist");
+    cy.findByText(/Average GPA/).should("exist");
   });
 
-  it("has a checkout section with expected form inputs", () => {
-    cy.findByText("Checkout").should("exist");
-    cy.get("form").findByLabelText("First Name").should("exist");
-    cy.get("form").findByLabelText("Last Name").should("exist");
+  it("has a Teacher enrollment section with expected form inputs", () => {
+    cy.findByText("Teacher Enrollment").should("exist");
+    cy.get("form").findByLabelText("Name").should("exist");
+    cy.get("form").findByLabelText("Subject").should("exist");
     cy.get("form").findByLabelText("Email").should("exist");
-    cy.get("form").findByLabelText("Credit Card").should("exist");
+    cy.get("form").findByLabelText("Phone Number").should("exist");
     cy.get("form").findByLabelText("Zip Code").should("exist");
-    cy.get("button").contains("Buy Now").should("exist");
+    cy.get("button").contains("Teach Class").should("exist");
   });
 });
